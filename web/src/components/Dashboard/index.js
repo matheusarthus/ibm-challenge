0133722ea60b34d683cef54860c034202866385f
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-09-18 23:55:03 -0300

    Answers card with data from StackOverflow

--- web/src/components/Dashboard/index.js
+++ web/src/components/Dashboard/index.js
@@ -1,13 +1,34 @@
 import React, { useState } from 'react';
-
-import { Container, Header, Form, Logo } from './styles';
+import { toDate, format } from 'date-fns';
+import axios from 'axios';
+import {
+  Container,
+  Header,
+  Form,
+  Logo,
+  AnswersContainer,
+  Answer,
+} from './styles';
 
 function Dashboard({ user }) {
   const [question, setQuestion] = useState('');
+  const [answers, setAnswers] = useState([]);
 
   const logOut = () => {
     // window.open('http://localhost:3333/auth/logout', '_self');
-    console.log(question);
+    console.log(answers);
+  };
+
+  const searchQuestion = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=${question}&site=stackoverflow`
+      );
+
+      setAnswers(response.data.items);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -16,7 +37,16 @@ function Dashboard({ user }) {
         <div>
           <Logo>This Error will be fine.</Logo>
         </div>
-        <div>
+        <div className="buttons">
+          <button
+            id="errors"
+            type="button"
+            onClick={() => {
+              logOut();
+            }}
+          >
+            my Errors
+          </button>
           <button
             type="button"
             onClick={() => {
@@ -30,14 +60,74 @@ function Dashboard({ user }) {
       <Container>
         <Form>
           <h2>{`Hello, ${user && user.username.split(' ')[0]}.`}</h2>
-          <input
-            placeholder="What is your ERROR?"
-            onChange={(item) => {
-              setQuestion(item.target.value);
-            }}
-          />
-          <button type="button">GO!</button>
+          <div>
+            <input
+              placeholder="What is your ERROR?"
+              onChange={(item) => {
+                setQuestion(item.target.value);
+              }}
+            />
+            <button
+              type="button"
+              onClick={() => {
+                searchQuestion();
+              }}
+            >
+              GO!
+            </button>
+          </div>
         </Form>
+        <AnswersContainer>
+          {answers &&
+            answers.map((answer) => (
+              <Answer
+                key={answer.question_id}
+                onClick={() => {
+                  console.log('teste');
+                }}
+              >
+                <h4>{answer.title}</h4>
+                <div>
+                  <div id="metrics">
+                    <strong>Respostas:</strong>
+                    <span>{answer.answer_count}</span>
+                  </div>
+                  <div id="metrics">
+                    <strong>Votos:</strong>
+                    <span>{answer.score}</span>
+                  </div>
+                  <div id="metrics">
+                    <strong>Visitas:</strong>
+                    <span>{answer.view_count}</span>
+                  </div>
+                </div>
+                <div className="tags">
+                  {answer.tags &&
+                    answer.tags.map((tag) => (
+                      <div id="tag" key={tag}>
+                        <span>{tag}</span>
+                      </div>
+                    ))}
+                </div>
+                <div className="dates">
+                  <strong>Criação:</strong>
+                  <span>
+                    {format(
+                      new Date(answer.creation_date * 1000),
+                      'dd/MM/yyyy - HH:mm:ss'
+                    )}
+                  </span>
+                  <strong>Atualização:</strong>
+                  <span>
+                    {format(
+                      new Date(answer.last_activity_date * 1000),
+                      'dd/MM/yyyy - HH:mm:ss'
+                    )}
+                  </span>
+                </div>
+              </Answer>
+            ))}
+        </AnswersContainer>
       </Container>
     </>
   );
--- web/src/components/Dashboard/styles.js
+++ web/src/components/Dashboard/styles.js
@@ -19,11 +19,48 @@ export const Header = styled.div`
   background-color: #222;
   padding: 10px;
 
-  button {
-    padding: 5px 10px;
-    background-color: #222;
-    border: 3px solid rgba(255, 255, 255);
-    color: #fff;
+  div.buttons {
+    display: flex;
+
+    @media (max-width: 500px) {
+      flex-direction: column;
+    }
+
+    button {
+      padding: 5px 10px;
+      background-color: #222;
+      border: 3px solid rgba(255, 255, 255);
+      color: #fff;
+      font-weight: bold;
+      margin-left: 10px;
+      width: 90px;
+
+      &:hover {
+        background: #fff;
+        color: #222;
+      }
+
+      @media (max-width: 500px) {
+        font-size: 12px;
+      }
+    }
+
+    button#errors {
+      padding: 5px 10px;
+      background-color: #f00;
+      border: 3px solid rgba(255, 255, 255);
+      color: #fff;
+      font-weight: bold;
+
+      @media (max-width: 500px) {
+        margin-bottom: 5px;
+      }
+
+      &:hover {
+        background: #fff;
+        color: #222;
+      }
+    }
   }
 `;
 
@@ -33,6 +70,10 @@ export const Logo = styled.span`
   font-size: 22px;
   margin-bottom: 20px;
   color: #fff;
+
+  @media (max-width: 500px) {
+    font-size: 18px;
+  }
 `;
 
 export const Form = styled.div`
@@ -50,6 +91,10 @@ export const Form = styled.div`
     border: 3px solid rgba(0, 0, 0);
     padding: 5px 10px;
 
+    @media (max-width: 500px) {
+      min-width: 180px;
+    }
+
     ::placeholder {
       font-size: 16px;
       text-align: center;
@@ -57,10 +102,95 @@ export const Form = styled.div`
   }
 
   button {
+    margin-left: 5px;
     padding: 5px 10px;
     background-color: #aaa;
     border: 3px solid rgba(0, 0, 0);
     color: #222222;
     font-weight: bold;
+
+    &:hover {
+      background: #222;
+      color: #fff;
+    }
+  }
+`;
+
+export const AnswersContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Answer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  border: 3px solid rgba(0, 0, 0);
+  width: 60%;
+  padding: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+
+  @media (max-width: 720px) {
+    width: 90%;
+  }
+
+  &:hover {
+    background: #888;
+    color: #fff;
+  }
+
+  h4 {
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    margin-top: 5px;
+
+    @media (max-width: 500px) {
+      flex-direction: column;
+    }
+
+    div#metrics {
+      display: flex;
+      flex-direction: row;
+      align-content: center;
+      align-items: center;
+    }
+
+    strong {
+      font-size: 12px;
+      font-weight: bold;
+    }
+
+    span {
+      margin-left: 2px;
+      margin-right: 10px;
+    }
+  }
+
+  div.tags {
+    display: flex;
+
+    @media (max-width: 500px) {
+      flex-direction: column;
+    }
+
+    div#tag {
+      display: flex;
+      justify-content: center;
+      align-content: center;
+      align-items: center;
+      background-color: #1da1f2;
+      margin-right: 5px;
+      margin-bottom: 10px;
+      color: #fff;
+      padding: 3px;
+    }
   }
 `;
