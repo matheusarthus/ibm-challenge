ee866d494a730c43ca924a35bddd4c250f047308
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-09-19 21:17:02 -0300

    Fix: no utils variables

--- web/src/components/Dashboard/index.js
+++ web/src/components/Dashboard/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import api from '../../services/api';
 
